28afefd2ed5e96ba1677be4c48236cd69d6e93a6
nirob <nirobprottoy12@gmail.com>
2026-04-07 21:01:00 +0600

    solve the mistake of update controller...instead of getting object we define the array.

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,19 +1,15 @@
-import User from "../models/user.models.js"; 
+import User from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js"; 
-import { asyncHandler } from "../utils/asyncHandler.js"; 
-
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 // Update Cart
 export const updateCart = asyncHandler(async (req, res) => {
-  const { userId, cartItems } = req.body; 
+  const { cartItems } = req.body;
+  const userId = req.user._id;
 
-  if (!cartItems || !Array.isArray(cartItems)) {
-    throw new ApiError(400, "Valid cart items array is required");
-  }
-
-  if (!userId) {
-    throw new ApiError(400, "User ID is required");
+  if (!cartItems || typeof cartItems !== "object" || Array.isArray(cartItems)) {
+    throw new ApiError(400, "Valid cart items object is required");
   }
 
   const cartUpdated = await User.findByIdAndUpdate(
@@ -22,13 +18,13 @@ export const updateCart = asyncHandler(async (req, res) => {
       $set: { cartItems },
     },
     {
-      new: true, // 
-      runValidators: true, 
+      new: true,
+      runValidators: true,
     },
   );
 
   if (!cartUpdated) {
-    throw new ApiError(404, "User not found"); 
+    throw new ApiError(404, "User not found");
   }
 
   return res
