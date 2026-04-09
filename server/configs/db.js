f66177dd1adf355e2fe2bf75193394308bdf2c92
nirob <nirobprottoy12@gmail.com>
2026-04-09 22:03:32 +0600

    New DB Connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,23 +1,19 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  if (!process.env.MONGODB_URI) {
-    console.error("❌ Error: MONGODB_URI is not defined in .env file");
-    process.exit(1);
+  if (mongoose.connection.readyState >= 1) {
+    console.log("Using existing MongoDB connection");
+    return;
   }
 
   try {
-    // 2. Await Connection & Log Host
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/grofresh`,
-    );
-
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
     console.log(
-      `✅ Database Connected Successfully! DB Host: ${connectionInstance.connection.host}`,
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`,
     );
   } catch (error) {
-    console.error("❌ MONGODB connection FAILED: ", error);
-    process.exit(1);
+    console.log("MONGODB connection FAILED ", error);
+    throw error;
   }
 };
 
